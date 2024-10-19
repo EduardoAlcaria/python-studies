be53d443a8688967943683ce4f26bbc6ea5f1dac
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-19 17:33:53 -0300

    input an integer so plus them

--- classes/inputplus.py
+++ classes/inputplus.py
@@ -1,4 +1,4 @@
-a = int(input("Type a number: "))
-b = int(input("Type another number: "))
+x = int(input("Type a num: "))
+y = int(input("Type another num: "))
 
-print("The result between {0} plus {1} is {2}".format(a, b, a+b))
+print("The result between {0} plus {1} is {2}".format(x, y, x+y))
