bdc046a52b451f64a12c9f46e2bbea29c39a8a71
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-23 14:44:18 -0300

    studies about primitive types

---
+++ classes/primitiveV.py
@@ -0,0 +1,9 @@
+n1 = int(input('Type a number: '))
+n2 = int(input('Type another number: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+p = n2 ** n2
+print('the sum is {}, the times is {} and the division is {}'.format(s, m, d), end=' ')
+print('The remainder is {} and the power is {}'. format(di, p))
