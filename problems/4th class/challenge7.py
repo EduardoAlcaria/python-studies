4c309402284fa8a196493e633b1e7e76e3fa620e
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-24 19:33:51 -0300

    challenge 7 about do the math for a minimum grade

--- problems/4th class/challenge7.py
+++ problems/4th class/challenge7.py
@@ -1,3 +1,3 @@
-n1 = int(input('Type the 1st grade: '))
-n2 = int(input('Type the 2nd grade: '))
-print('The minimum grade is: {}'. format((n1+n2)/2))
+n1 = float(input('Type the 1st grade: '))
+n2 = float(input('Type the 2nd grade: '))
+print('The minimum grade is: {:.1f}'. format((n1+n2)/2))
