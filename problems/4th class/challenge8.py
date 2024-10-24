c9316b20af3870a43ee46de0b6e0c0a16041d78f
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-24 19:51:53 -0300

    challenge 8 about read a measure in meters and shows the convertion to centimeters and millimeters

--- problems/4th class/challenge8.py
+++ problems/4th class/challenge8.py
@@ -1,2 +1,2 @@
 x = float(input('type a measure in meters: '))
-print('CM: {}\nMM: {}\n'.format(x * 100, x * 1000))
+print('KM: {0}\nHM: {1}\nDAM: {2}\nDM: {3}\nCM: {4}\nMM: {5}\n'.format(x / 1000, x / 100, x / 10, x * 10, x * 100, x * 1000))
