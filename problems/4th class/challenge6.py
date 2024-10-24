3566c86b2a9df3c27f700405131b375c3facec68
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-24 19:24:32 -0300

    challenge 6 about show the double, the triple and the square root of a number

--- problems/4th class/challenge6.py
+++ problems/4th class/challenge6.py
@@ -1,2 +1,2 @@
 n1 = int(input('Type a number: '))
-print('double: {}\ntriple: {}\nsquare root: {}\n'.format(n1 * 2, n1 * 3, n1**(1/2)))
+print('double: {}\ntriple: {}\nsquare root: {:.2f}\n'.format(n1 * 2, n1 * 3, n1**(1/2)))
