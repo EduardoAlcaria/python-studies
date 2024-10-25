e79eb6d16a65b0da90ec3724c6d655ebb7d127b3
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-25 12:30:10 -0300

    challenge 11 about do the math for how many liters of paint i need to cover a wall

--- problems/4th class/challenge11.py
+++ problems/4th class/challenge11.py
@@ -1,4 +1,4 @@
 height = float(input('Height: '))
 length = float(input('Length: '))
 area = length * height
-print('The total of liters are {:.2}'. format(area / 32))
+print('The total of liters are {}'. format(area / 2))
