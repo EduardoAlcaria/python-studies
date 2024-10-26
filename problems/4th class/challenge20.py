ce33e4809fa8d18ea4e6eabcf1faec9185299737
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-26 11:48:20 -0300

    challenge 20 about shuffe an array and display the new order

--- problems/4th class/challenge20.py
+++ problems/4th class/challenge20.py
@@ -1,6 +1,8 @@
-import random
-names = ["Eduardo", "Artur", "Miguel", "Pedro"]
-print('1st {}'.format(random.choice(names)))
-print('2nd {}'.format(random.choice(names)))
-print('3th {}'.format(random.choice(names)))
-print('4th {}'.format(random.choice(names)))
+from random import shuffle
+n1 = str(input('1st name: '))
+n2 = str(input('2nd name: '))
+n3 = str(input('3th name: '))
+n4 = str(input('4th name: '))
+names = [n1, n2, n3, n4]
+shuffle(names)
+print('the order will be\n{}'.format(names))
