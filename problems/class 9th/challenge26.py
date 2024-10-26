7ecb8a662845facd954a460006d7dabceeedc632
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-26 17:36:57 -0300

    challenge 26 about read a string and show how many letters A there is and which spot the first and last letter A is

---
+++ problems/class 9th/challenge26.py
@@ -0,0 +1,4 @@
+string = str(input('Type something: '))
+print('There is {} letters A'.format(string.lower().count('a')))
+print('The first letter A are in the spot {}'.format(string.find('a')))
+print('The last letter A are ins the spot {}'.format(string.rfind('a')))
