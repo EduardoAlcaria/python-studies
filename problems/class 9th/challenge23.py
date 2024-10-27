0c4fefb602fb2dd345d3447aed1629482abcb866
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-27 17:07:14 -0300

    challenge 23 about read a number and thear it down shown the ones, tens, hundreds and thousands

--- problems/class 9th/challenge23.py
+++ problems/class 9th/challenge23.py
@@ -1,6 +1,9 @@
-num = str(input('Type a number (from 0 to 9999): '))
-print('{} Ones'.format(num[3]))
-print('{} Tens'.format(num[2]))
-print('{} Hundreds'.format(num[1]))
-print('{} Thousands'.format(num[0]))
-
+num = int(input('Type a number (from 0 to 9999): '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('{} Ones'.format(u))
+print('{} Tens'.format(d))
+print('{} Hundreds'.format(c))
+print('{} Thousands'.format(m))
