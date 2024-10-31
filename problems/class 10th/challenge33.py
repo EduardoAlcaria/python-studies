e7f22ee94fd645cb3e07560b1ef82acffe4d7846
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-31 17:05:07 -0300

    challenge 33 about read 3 numbers and see which one is the lowest and the highest

--- problems/class 10th/challenge33.py
+++ problems/class 10th/challenge33.py
@@ -1,3 +1,20 @@
-n1 = int(input('Type the 1st number: '))
-n2 = int(input('Type the 2nd number: '))
-n3 = int(input('Type the 3th number: '))
+a = int(input('Type the 1st number: '))
+b = int(input('Type the 2nd number: '))
+c = int(input('Type the 3th number: '))
+low = a
+if b<a and b<c:
+    low = b
+if c<a and c<b:
+    low = c
+
+
+high = a
+if b>a and b>c:
+    high = b
+if c>b and c>a:
+    high = c
+
+
+print('the lowest number is {}'.format(low))
+print('the highest number is {}'.format(high))
+
