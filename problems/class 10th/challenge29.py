cf74f770476be1a7e2bb773c419edf5a6155b472
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-31 16:17:32 -0300

    challenge 29 about read an speed and see if its necessary to pay a fine

--- problems/class 10th/challenge29.py
+++ problems/class 10th/challenge29.py
@@ -1,5 +1,4 @@
 km = float(input('Type a speed KM: '))
 if km > 80:
     print('You have been fined on {:.2f}$'.format((km - 80) * 7)) 
-else:
-    print('Have a good day, drive safety!')
+print('Have a good day, drive safety!')
