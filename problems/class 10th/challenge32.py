460ba5d2413807dc168f5d31234c6d5d85184ee1
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-31 16:49:20 -0300

    challenge 32 about read a year and see if its a leap year

--- problems/class 10th/challenge32.py
+++ problems/class 10th/challenge32.py
@@ -1,5 +1,8 @@
-year = int(input('Type a year: '))
-if year%4 == 0:
-    print('Its a leap year')
+from datetime import date
+year = int(input('Type a year or type 0 to analize the current year: '))
+if year == 0:
+    year = date.today().year
+if year%4 == 0 and year%100 != 0 or year % 400 == 0:
+    print('the year {} is a leap year'.format(year))
 else:
-    print('Its a normal year')
+    print('the year {} is a normal year'.format(year))
