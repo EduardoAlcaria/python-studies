467a9467e8a3509f6b9b45cd2f7d955eeeacca26
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-31 13:35:47 -0300

    studies about conditionals

--- classes/if.py
+++ classes/if.py
@@ -1,3 +1,8 @@
-time = int(input('How many years your car have '))
-print('new car' if time <= 3 else 'old car')
- 
+n1 = float(input('Type the 1st grade: '))
+n2 = float(input('Type the 2nd grade: '))
+m = (n1+n2)/2
+print('your minimal grade are {:.1f}'.format(m))
+if m >= 6.0:
+    print('Your minimal grade is great')
+else:
+    print('Your minimal grade is the worse')
