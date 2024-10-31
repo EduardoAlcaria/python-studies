b6518940e4920dfdcc68dc9958aa3375307c3bb3
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-31 15:01:52 -0300

    challenge 35 about read measures and see if it can be a triangule

---
+++ problems/class 10th/challenge35.py
@@ -0,0 +1,7 @@
+n1 = float(input('Type a measure: '))
+n2 = float(input('Type a measure: '))
+n3 = float(input('Type a measure: '))
+if n1 + n2 > n3:
+    print('its a triangule')
+else:
+    print('its not a triangule')
