4d76ae07838c31709ebadbf8b386beb9ef2401cf
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-01 08:20:00 -0300

    challenge 34 about read a salary and see if it will be 10% or 15% bigger

--- problems/class 10th/challenge34.py
+++ problems/class 10th/challenge34.py
@@ -1,6 +1,6 @@
 s = float(input('Type a salary: '))
 aug = s * (10/100)
 if s <= 1250:
-    print('your new salary is 15 persent bigger\nand it will be ${}'.format(s * (15/100) + s))
+    print('your new salary is 15 persent bigger\nand it will be ${}'.format((s * 15/100) + s))
 else:
-    print('your new salary is 10 persent bigger\nand it will be ${}$'.format(s * (10/100) + s))
+    print('your new salary is 10 persent bigger\nand it will be ${}$'.format((s * 10/100) + s))
