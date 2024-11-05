8b494374df8e44f7a85b3f0432a343dc7f2e7862
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-05 17:15:21 -0300

    challenge 39 about read the age and find out if its necessary sign up for the army

---
+++ problems/class 12th/challenge39.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+born = int(input('Enter the year the you was born '))
+age = datetime.now().year - born
+if age == 18:
+    print('You need to sign up for the army')
+elif age > 18:
+    print('its late for sign up to the army')
+else:
+    print('You will sign up to the army in {} years'.format(18 - age))
