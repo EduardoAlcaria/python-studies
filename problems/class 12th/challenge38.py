03cff8b7fdee9fd996daff32815e8139963a4671
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-05 16:55:33 -0300

    challenge 38 about read a number and find out which one is bigger

---
+++ problems/class 12th/challenge38.py
@@ -0,0 +1,9 @@
+n1 = int(input('Type a number: '))
+n2 = int(input('Type another number: '))
+
+if n1 > n2:
+    print('The first number is bigger ')
+elif n2 > n1:
+    print('The second number is bigger ')
+else:
+    print('They are the same')
