7909aea36e9b04bf6b0162cdc119e5cbb94d6426
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-07 16:32:02 -0300

    challenge 45 about doing a jokenpo game

--- problems/class 12th/challenge45.py
+++ problems/class 12th/challenge45.py
@@ -2,30 +2,33 @@ from random import choice
 
 rpc_list = ["rock", "paper", "scissors"]
 pc = choice(rpc_list)
+green = "\033[1;32m"
+red = "\033[1;31m"
+reset = "\033[0m"
 
 play = str(input('what do you want to trow > '))
 #tie
 if play == pc:
-    print('its a tie')
+    print('\033[1;33mits a tie\033[0m')
 #COM
 elif play == 'rock' and pc == 'paper':
-    print('the COM has won')
+    print('{} the COM has won {}'.format(red, reset))
     print('the COM trew {}'.format(pc))
 elif play == 'paper' and pc == 'scissors':
-    print('the COM has won')
+    print('{} the COM has won {}'.format(red, reset))
     print('the COM trew {}'.format(pc))
 elif play == 'scissors' and pc == 'rock':
-    print('the COM has won')
+    print('{} the COM has won {}'.format(red, reset))
     print('the COM trew'.format(pc))
 #player
 elif play == 'paper' and pc == 'rock':
-    print('you have won')
+    print('{} you have won {}'.format(green, reset))
     print('the COM trew {}'.format(pc))
 elif play == 'scissors' and pc == 'paper':
-    print('you have won')
+    print('{} you have won {}'.format(green, reset))
     print('the COM trew {}'.format(pc))
 elif play == 'rock' and pc == 'scissors':
-    print('you have won')
+    print('{} you have won {}'.format(green, reset))
     print('the COM trew {}'.format(pc))
 else:
     print('you have typed something wrong')
