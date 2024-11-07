15623c2d827906dfd2884ccd035fb42119d57bc9
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-07 16:22:38 -0300

    challenge 36 about do the math for a house loan

--- problems/class 12th/challenge36.py
+++ problems/class 12th/challenge36.py
@@ -6,9 +6,8 @@ times =  houseValue / (years * 12)
 per = salary * 0.3
 
 
-if times < per:
+if times <= per:
     print('\nyour loan has been \033[1;32mAPPROVED\033[0m \nyou will pay in {:.2f}$ per month\n'.format(times))
-elif times > per:
-    print('your loan has been \033[1;31mDENNIED\033[0m\nyour salary doesnt cover the foan')
 else:
-    print('good bye')
+    print('your loan has been \033[1;31mDENNIED\033[0m\nyour salary doesnt cover the foan')
+    
