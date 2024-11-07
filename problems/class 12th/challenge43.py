d017a64dfce5223566d34ecfbcd981d717df7b87
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-07 14:19:12 -0300

    challenge 43 about read the weight and the height to do the BMI math

---
+++ problems/class 12th/challenge43.py
@@ -0,0 +1,13 @@
+weight = float(input('Whats your weight KG: '))
+height = float(input('whats your hieght M: '))
+
+bmi = weight/(height ** 2)
+print('your bmi is {:.2f}'.format(bmi))
+if bmi < 18.5:
+    print('underweight')
+elif bmi >= 18.5 and bmi < 25:
+    print('healthy weight')
+elif bmi >= 25 and bmi < 30:
+    print('overweight')
+elif bmi >= 30:
+    print('obese class')
