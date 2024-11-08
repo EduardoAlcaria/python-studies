908643e55ecbffb2f0ad2eeaf7daeceb36b7346a
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-08 16:11:16 -0300

    challenge 44 about read a price and shows the price with disconts and charges

--- problems/class 12th/challenge44.py
+++ problems/class 12th/challenge44.py
@@ -1,11 +1,16 @@
-price = int(input('Whats the price $'))
+price = float(input('how much have you spent $'))
 choise = int(input('You want to pay with\n[1] cash\n[2] in cash with credit card\n[3] in 2 times \n[4] in 3 times or more\n>'))
 
 if choise == 1:
-    print('You will pay {}$'.format(price - (10/price) * 100))
+    print('You will pay {:.2f}$'.format(price - (price * 10/100)))
 elif choise == 2:
-    print('You will pay {}$'.format(price - (5/price) * 100))
+    print('You will pay {:.2f}$'.format(price - (price * 5/100)))
 elif choise == 3:
-    print('You will pay the same price of {}$'.format(price))
+    print('You will pay the same price of {:.2f}$ in 2x of {}$'.format(price, price/2))
 elif choise == 4:
-    print('you will pay {}$ with 20% charged'.format((20/price) * 100 + price))
+    total = price + (price * 20/100)
+    times = int(input('How many times? '))
+    print('you will pay {:.2f}$ in {}x of {:.2f}$ with 20% charged'.format(total,times,total/times))
+
+else:
+    print('invalid payment option')
