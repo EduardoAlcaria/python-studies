4799bf4612447b8db61476dbdab88f4f9a05e553
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-08 14:05:54 -0300

    challenge 40 about read 2 grade and find out if you've been refuced, approved or need to do the recovery exams

--- problems/class 12th/challenge40.py
+++ problems/class 12th/challenge40.py
@@ -3,7 +3,7 @@ n2 = float(input('Type the 2nd grade: '))
 grade = (n1 + n2) / 2
 if grade <= 5.0:
     print('REFUCED\nGRADE: {}'.format(grade))
-elif grade > 5.0 and grade < 6.9:
+elif grade > 5.0 and grade <= 6.9:
     print('RECOVERY EXAMS\nGRADE: {}'.format(grade))
 else:
     print('APPROVED\nGRADE: {}'.format(grade))
