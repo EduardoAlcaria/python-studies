33f96eb75c91be099da544be10cf85bf3224654a
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-08 14:39:18 -0300

    challenge 42 about doing an upgrade of challenge 35 with a triangule  classify system

--- problems/class 12th/challenge42.py
+++ problems/class 12th/challenge42.py
@@ -2,13 +2,13 @@ n1 = float(input('Type a mesure: '))
 n2 = float(input('Type a mesure: '))
 n3 = float(input('Type a mesure: '))
 
-triangule_law = n1 + n2 < n3
+triangule_law = n1 < n2 + n3 and n2 < n1 + n3 and n3 < n1 + n2
 print_can = 'it can be a triangule'
 
-if n1 == n2 and n2 == n3 == n1 == n3 and triangule_law:
+if n1 == n2 and n2 == n3 and n1 == n3 and triangule_law:
     print(print_can)
     print('its an equilateral triangule')
-elif n1 == n2 and triangule_law: 
+elif n1 == n2 or n2 == n3 or n3 == n1 and triangule_law: 
     print(print_can)
     print('Its an isosceles triangule')
 elif n1 != n2 and n2 != n3 and n3 != n1 and triangule_law:
