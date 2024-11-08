391af6db2863cfe66ae5c21296051719a96358f2
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-08 13:18:53 -0300

    challenge 37 about read a number and show the binary, hex and octal version of it

--- problems/class 12th/challenge37.py
+++ problems/class 12th/challenge37.py
@@ -1,10 +1,10 @@
 n = int(input('Enter a number: '))
 choice = str(input('You want to conver the number into:\n [1] binary  [2] octal  [3] hexadecimal '))
 if choice == '1':
-    print('the binary is {}'.format(bin(n)))
+    print('the binary is {}'.format(bin(n)[2:]))
 elif choice == '2':
-    print('The octal is {}'.format(oct(n)))
+    print('The octal is {}'.format(oct(n)[2:]))
 elif choice == '3':
-    print('The hex is {}'.format(hex(n)))
+    print('The hex is {}'.format(hex(n)[2:]))
 else:
     print('bye')
