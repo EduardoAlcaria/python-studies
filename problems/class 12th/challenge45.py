e2fd71baa794ddd47c4275739fca6672f318f93c
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-08 16:54:52 -0300

    challenge 45 about doing a jokenpo game

--- problems/class 12th/challenge45.py
+++ problems/class 12th/challenge45.py
@@ -1,35 +1,33 @@
-from random import choice
-
-rpc_list = ["rock", "paper", "scissors"]
-pc = choice(rpc_list)
-
+from random import randint
 green = "\033[1;32m"
 red = "\033[1;31m"
 reset = "\033[0m"
 
-play = str(input('what do you want to trow > '))
-#tie
-if play == pc:
-    print('\033[1;33mits a tie\033[0m')
-#COM
-elif play == 'rock' and pc == 'paper':
-    print('{} the COM has won {}'.format(red, reset))
-    print('the COM trew {}'.format(pc))
-elif play == 'paper' and pc == 'scissors':
-    print('{} the COM has won {}'.format(red, reset))
-    print('the COM trew {}'.format(pc))
-elif play == 'scissors' and pc == 'rock':
-    print('{} the COM has won {}'.format(red, reset))
-    print('the COM trew'.format(pc))
-#player
-elif play == 'paper' and pc == 'rock':
-    print('{} you have won {}'.format(green, reset))
-    print('the COM trew {}'.format(pc))
-elif play == 'scissors' and pc == 'paper':
-    print('{} you have won {}'.format(green, reset))
-    print('the COM trew {}'.format(pc))
-elif play == 'rock' and pc == 'scissors':
-    print('{} you have won {}'.format(green, reset))
-    print('the COM trew {}'.format(pc))
+itens = ('rock', 'paper', 'scissors')
+pc = randint(0, 2)
+
+play = int(input("""what do you want to throw
+[0] rock
+[1] paper
+[2] scissors
+> """))
+
+if play not in [0, 1, 2]:
+    print('invalid choice')
 else:
-    print('you have typed something wrong')
+    print('you threw {}'.format(itens[play]))
+    print('the COM threw {}'.format(itens[pc]))
+
+    #tie
+    if itens[play] == itens[pc]:
+        print('\033[1;33mits a tie\033[0m')
+    #won
+    elif itens[play] == 'paper' and itens[pc] == 'rock':
+        print(green ,'You Won', reset)
+    elif itens[play] == 'scissors' and itens[pc] == 'paper':
+        print(green ,'You Won', reset)
+    elif itens[play] == 'rock' and itens[pc] == 'scissors':
+        print(green, 'you Won', reset)
+    #lost
+    else:
+        print(red, 'You Lost', reset)
