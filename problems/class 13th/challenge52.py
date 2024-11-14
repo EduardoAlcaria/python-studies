36161eddb64b0fcbccf66bdd28680c727befd61e
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-14 14:16:31 -0300

    challenge 52 about read a number and find out with its a prime number

---
+++ problems/class 13th/challenge52.py
@@ -0,0 +1,12 @@
+count = 0
+n = int(input("Type a number: "))
+for c in range(1, n+1):
+    if n % c == 0:
+        print("\033[032m",c,"\033[m", end=" ")
+        count += 1
+    else:
+        print("\033[031m",c,"\033[m", end=" ")
+if  count < 2:       
+    print(f"\n \033[1;32mIts a prime number\033[m")
+else:
+    print(f"\n \033[1;31mIts not a prime number\033[m")
--- problems/class 13th/challenge53.py
+++
@@ -1,5 +0,0 @@
-f = str(input("Type a sentence: ")).lower().replace(" ", "").strip()
-for c in range(len(f), 0, - 1):
-    print(f[c-len(f)-1],end="")
-
-
