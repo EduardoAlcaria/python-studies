d0a002edb9674fe501cde38dae3fa949c18a5612
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-14 13:45:13 -0300

    challenge 50 about read 6 numbers and do the summing if they are even

--- problems/class 13th/challenge50.py
+++ problems/class 13th/challenge50.py
@@ -1,6 +1,8 @@
 s = 0
+count = 0
 for c in range(1, 7):
     n = int(input(f"Type the {c} number: "))
     if n % 2 == 0:
         s += n
-print(f"the summing is {s}")
+        count += 1
+print(f"you have typed {count} even numbers and the summing is {s}")
