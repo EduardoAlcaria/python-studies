ee8789e9feff63523416820b0885323fbe6ae29c
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-14 13:25:24 -0300

    challenge 48 about to the summing for the odd numbers multiples of 3 from 1 to 500

--- problems/class 13th/challenge48.py
+++ problems/class 13th/challenge48.py
@@ -1,6 +1,8 @@
 s = 0
+count = 0
 for c in range(1, 500):
     if c % 2 == 1 and c % 3 == 0:
         s += c
-print(f"The sum of all odd numbers between 1 and 500 is {s}")
+        count += 1
+print(f"The sum of all {count} odd numbers between 1 and 500 is {s}")
 
