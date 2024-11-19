27acf33e5060070b50dde0fd85333554a576a449
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-19 16:15:28 -0300

    challenge 58 about doing a upgrade of challenge 028, now with the while loop

--- problems/class 14th/challenge58.py
+++ problems/class 14th/challenge58.py
@@ -3,10 +3,15 @@ com_random = randint(0, 10)
 player_guess = -1
 trys = 1
 while player_guess != com_random:
-    player_guess = int(input("Enter yout guess [0-10]: "))
+    player_guess = int(input("Enter your guess [0-10]: "))
     if player_guess == com_random:
         print(f"you have won with {trys} trys")
-    else:
-        print("wrong try again")
-        trys += 1
+    elif player_guess > com_random or player_guess < com_random:
+        if player_guess == 10 or player_guess > com_random:
+            print("Try a lower number")
+            trys += 1
+        else:
+            print("Try a higher number")
+            trys += 1
+    
 
