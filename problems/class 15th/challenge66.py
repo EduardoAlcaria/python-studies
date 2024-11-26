b23b7b05cc61c622bd8a2451cf04d971927a757e
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-26 15:39:36 -0300

    challenge 66 about remaking the challenge 65 but now with the loop breaker and while true

--- problems/class 15th/challenge66.py
+++ problems/class 15th/challenge66.py
@@ -1,8 +1,10 @@
 n = 0
 s = 0
+count = 0
 while True:
     n = int(input("Type a number [type 999 to stop]: "))
     if n == 999:
         break
     s += n
-print(f"the sum is {s}")
+    count += 1
+print(f"you have typed {count} numbers and the sum is {s}")
