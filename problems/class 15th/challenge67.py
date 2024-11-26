e7e3619c3217566990282ebbed77a02ee60d980e
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-26 16:18:42 -0300

    challenge 67 about remaking the multiplication table challenge with a loop breaker when the user type a negative number

--- problems/class 15th/challenge67.py
+++ problems/class 15th/challenge67.py
@@ -1,9 +1,7 @@
 while True:
     n = int(input("Which multiplication table do you want to see: "))
-    print("_"*10)
     if n < 0:
         break
     for x in range(1,11):
         print(f"{n} x {x} = {n*x}")
-    print("_"*60)
 print(f"End of the multiplication table program. Good Bye!")
