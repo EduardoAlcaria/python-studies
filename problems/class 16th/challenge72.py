2fc5ca54922ae75ba58b2db0f24ac757acbb4963
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-11-29 14:56:15 -0300

    challenge 72 about searching for a number between 0 and 20, using tuples

--- problems/class 16th/challenge72.py
+++ problems/class 16th/challenge72.py
@@ -1,9 +1,13 @@
 number = ("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen", "twenty")
 while True:
-    verify_number = int(input("type a number between 0 and 20: "))
-    if verify_number >= 0 and verify_number <= 20:
-        print(number[verify_number])
-        break
-    else: 
-        print("pleace, ", end="")
-        
+        ask_continue = " "
+        while ask_continue not in "yn":
+            verify_number = int(input("type a number between 0 and 20: "))
+            if verify_number >= 0 and verify_number <= 20:
+                print(f"you have typed the number {number[verify_number].upper()}")
+                break
+            else:
+                print("pleace, ", end="")
+        ask_continue = str(input("Do you want to continue [y/n]? ")).lower().strip()
+        if ask_continue == "n":
+             break
