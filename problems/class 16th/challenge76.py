77a9e90ed83df11ba2c0a2d2ef549ab687253823
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-12-03 14:27:33 -0300

    challenge 76 about doing a single tuple for keep integers and strings, and then list them in a sheet

--- problems/class 16th/challenge76.py
+++ problems/class 16th/challenge76.py
@@ -2,9 +2,9 @@ products = ("pen", 3,
             "notebook", 2000, 
             "apple", 4)
 print("_"*45)
-print("{:^45}".format("PRODUCT LIST"))
+print("{:^35}".format("PRODUCT LIST"))
 print("_"*45)
 for c in range(0, len(products), 2):
-    print("{:.<20}".format(products[c]), end="")
+    print("{:.<30}".format(products[c]), end="")
     print(f"${products[c+1]:.2f}")
 print("_"*45)
