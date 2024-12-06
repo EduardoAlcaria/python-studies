c8d2e1a9fdef38693fbcba8a4b768a67fb06ef4e
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-06 16:37:29 -0300

    challenge 81 about doing an analysis with a few number and shows how many number the user have typed, the descenfing order of them, and if the number 5 is in the list

--- problems/class 17th/challenge81.py
+++ problems/class 17th/challenge81.py
@@ -2,8 +2,8 @@ num = []
 continue_answer = " "
 while continue_answer not in "nN":
     num.append(int(input("The a number: ")))
-    continue_answer = str(input("Want to continue? [y/n]")).strip()
-print(f"you have typed {len(num)}")
+    continue_answer = str(input("Want to continue? [y/n] ")).strip()
+print(f"you have typed {len(num)} elements")
 print(f"the descending or of them is {sorted(num,reverse=True)}")
 if 5 in num:
     print("the number 5 is in the list")
