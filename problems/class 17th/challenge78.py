53dc79d6684b2e61c973a487814c1e016b146337
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-06 14:00:28 -0300

    challenge 78 was about doing an array with number and analyse with number in the higher and the lower and shows its position

--- problems/class 17th/challenge78.py
+++ problems/class 17th/challenge78.py
@@ -1,6 +1,23 @@
 num = []
-for v in range(0, 5):
-    num.append(int(input(f"Type the value {v+1}: ")))
-print(num)
-print(f"the highest number is {max(num)}, and its position is {num.index(max(num))}")
-print(f"the lower number is {min(num)}, and its position is {num.index(min(num))}")
+higher = 0
+lower = 0
+for c in range(0,5):
+    num.append(int(input(f"Type the {c+1} number: ")))
+    if c == 0:
+        higher = num[c]
+        lower = num[c]
+    else:
+        if num[c] > higher:
+            higher = num[c]
+        if num[c] < lower:
+            lower = num[c]
+print(f"the sequence is {num}")
+print(f"the highest number is {higher} and its position is ", end="")
+for i, c in enumerate(num):
+    if c == higher:
+        print(f"{i}... ",end="")
+print(f"\nthe lowest number is {lower} and its position is ",end="")
+for i, c in enumerate(num):
+    if c == lower:
+        print(f"{i}... ", end="")
+print()
