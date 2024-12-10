645df91d0d1587ce09ad6338048d38333f680763
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-10 19:52:20 -0300

    challenge 80 was about sorting a an array without using the sort method

--- problems/class 17th/challenge80.py
+++ problems/class 17th/challenge80.py
@@ -3,11 +3,13 @@ for c in range(0, 5):
     n = int(input("Type a number: "))
     if c == 0 or n > num[-1]:
         num.append(n)
+        print("add into the last position")
     else:
         pos = 0
         while pos < len(num):
             if n <= num[pos]:
                 num.insert(pos, n)
+                print(f"add into the {pos}")
                 break
             pos += 1
 print(num)
