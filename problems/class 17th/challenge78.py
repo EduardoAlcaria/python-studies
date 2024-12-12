1c9b87421fde197e57fe526fa0f3cfdce42c78af
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-12 13:43:39 -0300

    challenge 78 was about doing an array with number and analyse with number in the higher and the lower and shows its position

--- problems/class 17th/challenge78.py
+++ problems/class 17th/challenge78.py
@@ -1,23 +1,23 @@
 num = []
 higher = 0
 lower = 0
-for c in range(0, 5):
-    num.append(int(input(f"Type the [{c+1}/5] number: ")))
-    if c == 0:
-        higher = num[c]
-        lower = num[c]
+for n in range(0,5):
+    num.append(int(input(f"Type the {n+1}/5: ")))
+    if n == 0:
+        higher = num[n]
+        lower = num[n]
     else:
-        if num[c] > higher:
-            higher = num[c]
-        if num[c] < lower:
-            lower = num[c]
-print(f"The sequence is {num}")
-print(f"The lowest number is {lower} and its positon is: ",end="")
-for e,l in enumerate(num):
-    if l == lower:
-        print(f"{e}... ",end="")
-print(f"\nthe highest number is {higher} and its position is: ",end="")
-for e, h in enumerate(num):
+        if num[n] > higher:
+            higher = num[n]
+        if num[n] < lower:
+            lower = num[n]
+print(f"the sequence is {sorted(num)}")
+print(f"and higher value is {higher} and its position is: ",end="")
+for e, h  in enumerate(num):
     if h == higher:
-        print(f"{e}... ",end="")
+        print(f"{e}...",end=" ")
+print(f"\nand the lower value is {lower} and its position is: ",end="")
+for e, l in enumerate(num):
+    if l == lower:
+        print(f"{e}...",end=" ")
 print()
