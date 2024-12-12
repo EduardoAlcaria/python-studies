33bd4c43c4e2bcbd9472733bf91df15744ab33af
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-12 14:01:56 -0300

    challenge 82 was about reading a few numbers and keep them into 3 types of list, the numbers list, the even and the odd list, and them show to the user

--- problems/class 17th/challenge82.py
+++ problems/class 17th/challenge82.py
@@ -1,15 +1,16 @@
 num = []
 even = []
 odd = []
-continue_answer = " "
-while continue_answer not in "nN":
+while True:
     num.append(int(input("Type a number: ")))
-    continue_answer = str(input("Want to continue? [y/n] ")).strip()
-for c in range(0, len(num)):
-    if num[c] % 2 == 0:
-        even.append(num[c])
-    if num[c] % 2 == 1:
-        odd.append(num[c])
-print(f"the complete list are {num}")
-print(f"the list of even numbers are {even}")
-print(f"the list of odd numbers are {odd}")
+    continue_answer = str(input("Want to continue? [y/n]: ")).strip()
+    if continue_answer in "nN":
+        break
+for v in range(len(num)):
+    if num[v] % 2 == 0:
+        even.append(num[v])
+    if num[v] % 2 == 1:
+        odd.append(num[v])
+print(f"You have typed this sequence {sorted(num)}")
+print(f"The odd numbers are {odd}")
+print(f"And the even numbers are {even}")
