f62b4d5f962ab9e48c66aab29e5b9cfa8f7628d7
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-18 08:57:07 -0300

    challenge 87 about doing an upgrade of the challenge 86 but now showing the sum from all 3th coloum numbers, the sum of the even numbers, and the highest number of the 2nd coloum

--- problems/class 18th/challenge87.py
+++ problems/class 18th/challenge87.py
@@ -1,1 +1,13 @@
-matrix = [[], [], []]
+matrix = [[0,0,0], [0,0,0], [0,0,0]]
+sEven = sColomn = 0
+for l in range(3):
+    for c in range(3):
+        matrix[l][c] = int(input(f"Type a number for the [{l}, {c}] position: "))
+        if matrix[l][c] % 2 == 0:
+            sEven += matrix[l][c]
+        sColomn += matrix[l][2]
+for m in matrix:
+    print(f" [{m[0]:^5}] [{m[1]:^5}] [{m[2]:^5}]")
+print(f"the sum between all the even numbers are {sEven}")
+print(f"the highest number on the 2nd line is {max(matrix[1])}")
+print(f"the sum of all numbers on the 3th colomn is {sColomn}")
