1e91fb347c95811aad4652fba54b803b8dbe23dd
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-18 07:59:38 -0300

    challenge 85 about doing a single 2d array with even and odd numbers and then show them

--- problems/class 18th/challenge85.py
+++ problems/class 18th/challenge85.py
@@ -1,15 +1,10 @@
-num = []
-even = list()
-odd = list()
-for c in range(0, 7):
-    n = int(input(f"Type the {c+1}/7 number: "))
-    if n % 2 == 1:     
-        even.append(n)
-    if n % 2 == 0:
-        odd.append(n)
-num.append(odd[:])
-num.append(even[:])
-even.clear()
-odd.clear()
+num = [[], []]
+for c in range(1, 8):
+    n = int(input(f"Type the {c}/7 number: "))
+    if n % 2 == 0:     
+        num[0].append(n)
+    if n % 2 == 1:
+        num[1].append(n)
+print(num)
 print(f"the even values are {sorted(num[0])}")
 print(f"the odd values are {sorted(num[1])}")
