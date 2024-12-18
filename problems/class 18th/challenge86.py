ba4a96a4ac66659316e91ee07a91a32094316b60
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-18 08:34:38 -0300

    challenge 86 about reading a few numbers and put them into a matrix

--- problems/class 18th/challenge86.py
+++ problems/class 18th/challenge86.py
@@ -1,18 +1,6 @@
-l1 = []
-l2 = []
-l3 = []
-matrix = []
-for c in range(0, 3):
-    l1.append(int(input(f"type a num to [0,{c}]: ")))
-for c in range(0, 3):
-    l2.append(int(input(f"type a num to [1,{c}]: ")))
-for c in range(0, 3):
-    l3.append(int(input(f"type a num to [2,{c}]: ")))
-matrix.append(l1[:])
-l1.clear()
-matrix.append(l2[:])
-l2.clear()
-matrix.append(l3[:])
-l3.clear()
+matrix = [[0,0,0], [0,0,0], [0,0,0]]
+for l in range(3):
+    for c in range(3):
+        matrix[l][c] = int(input(f"Type a number for [{l}, {c}] position: "))
 for m in matrix:
-    print(f"[ {m[0]} ] [ {m[1]} ] [ {m[2]} ]")
+    print(f"[ {m[0]:^5} ] [ {m[1]:^5} ] [ {m[2]:^5} ]")
