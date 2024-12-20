923e3744bfddba752d62899ea8880a254b000854
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-20 14:48:27 -0300

    challenge 93 about doing a soccer player scores analyser

--- problems/class 19th/challenge93.py
+++ problems/class 19th/challenge93.py
@@ -8,9 +8,11 @@ for m in range(matches):
     player_data['goals'] = score[:]
     player_data['total'] = sum(score)
 print("-=" * 30)
+print(player_data)
+print("-=" * 30)
 for k, v in player_data.items():
     print(f"{k} has the value {v}")
 print("-=" * 30)
-print(f"the player {player_data['name']} has played {matches} matches")
+print(f"the player {player_data['name']} has played {len(player_data['goals'])} matches")
 for e,g in enumerate(score):
     print(f"      => On the {e} match he scored {g}")
