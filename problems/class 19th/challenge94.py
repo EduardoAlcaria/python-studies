77949fa4b29e76d72a0b1421fe5d713772f0d9d3
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-20 16:11:46 -0300

    challenge 94 about doing a group analyser with dictionaries and arrays

--- problems/class 19th/challenge94.py
+++ problems/class 19th/challenge94.py
@@ -3,31 +3,32 @@ data = {}
 avarege_age = 0
 while True:
     data["name"] = str(input("Name: "))
-    data["gen"] = str(input("Gen: [m/f]: "))
-    while data["gen"] not in "mFfM":
-        print("ERROR! type only M or F") 
-        data["gen"] = str(input("Gen: [m/f]: "))
+    while True:
+        data["gen"] = str(input("Gen [m/f]: "))[0]
+        if data["gen"] in "mfFM":
+            break
+        print("ERROR! Pleace type only M or F")
     data["age"] = int(input("Age: "))
     peoples.append(data.copy())
     data.clear()
-    ask_continue = str(input("Continue? [y/n]: ")).strip()
-    while ask_continue not in "yYnN":
-        print("ERROR! type only Y or N")
-        ask_continue = str(input("Continue? [y/n]: ")).strip()
+    while True:
+        ask_continue = str(input("Continue? [y/n]: "))[0]
+        if ask_continue in "yYnN":
+            break
+        print("ERROR! Pleace type only Y or N")
     if ask_continue in "nN":
         break
 for v in peoples:
     avarege_age += v['age']
 avarege_age = avarege_age/len(peoples)
-print("-=" * 30)
 print(f"- You have typed {len(peoples)} peoples")
 print(f"- The avarege age is {avarege_age:.2f}")
 print(f"- The womans in the list are: ",end="")
 for w in peoples:
-    if w["gen"] == "f":
+    if w["gen"] in "fF":
         print(w["name"],end=" ")
 print("\n- Peoples that are above the avarege age: ")
 for p in peoples:
-    if p["age"] > avarege_age:
+    if p["age"] >= avarege_age:
         print(f"   => name = {p['name']}; gen = {p['gen']}; age = {p['age']}")
 print("<< END >>")
