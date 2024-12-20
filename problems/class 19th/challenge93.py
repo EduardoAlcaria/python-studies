dd29af3716ebb281120bdf9f7a75321ba81fd460
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-20 14:44:16 -0300

    challenge 93 about doing a soccer player scores analyser

--- problems/class 19th/challenge93.py
+++ problems/class 19th/challenge93.py
@@ -1,14 +1,12 @@
 player_data = dict()
 score = list()
-tot = 0
 player_data['name'] = str(input("Soccer player name: "))
 matches = int(input("How many matches does he played: "))
 for m in range(matches):
     goal = int(input(f"How many goals did he score on {m} match: "))
-    tot += goal
     score.append(goal)
     player_data['goals'] = score[:]
-    player_data['total'] = tot
+    player_data['total'] = sum(score)
 print("-=" * 30)
 for k, v in player_data.items():
     print(f"{k} has the value {v}")
