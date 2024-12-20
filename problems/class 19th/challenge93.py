4915d4f4a192fcd3ecf327c85978564fe98b2920
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-20 14:27:14 -0300

    challenge 93 about doing a group analyser with dictionaries and arrays

--- problems/class 19th/challenge93.py
+++ problems/class 19th/challenge93.py
@@ -1,21 +1,18 @@
-player = dict()
+player_data = dict()
 score = list()
-score_sum = 0
-player["name"] = str(input("Soccer player: "))
-matches  = int(input("How many matches does he played? "))
-for m in range(0, matches):
-    score.append(int(input(f"match {m} How many points? ")))
-    score_sum += score[m]
-player["score"] = score[:]
-player["score_sum"] = score_sum
-score.clear()
+tot = 0
+player_data['name'] = str(input("Soccer player name: "))
+matches = int(input("How many matches does he played: "))
+for m in range(matches):
+    goal = int(input(f"How many goals did he score on {m} match: "))
+    tot += goal
+    score.append(goal)
+    player_data['goals'] = score[:]
+    player_data['total'] = tot
 print("-=" * 30)
-print(player)
+for k, v in player_data.items():
+    print(f"{k} has the value {v}")
 print("-=" * 30)
-for i, v in player.items():
-    print(f"The field {i} has the value {v}")
-print("-=" * 30)
-print(f"the player {player['name']} played {matches} matches")
-for e,g in enumerate(player["score"]):
-    print(f"     => At {e} match, he scored {g}")
-print(f"The total score was {score_sum} goals")
+print(f"the player {player_data['name']} has played {matches} matches")
+for e,g in enumerate(score):
+    print(f"      => On the {e} match he scored {g}")
