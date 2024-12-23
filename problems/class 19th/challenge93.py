5c1e2891453e62c181f8d3de8d1024765103b64b
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-23 12:37:57 -0300

    Remaking challenge 93 about doing a soccer player scores analyser

--- problems/class 19th/challenge93.py
+++ problems/class 19th/challenge93.py
@@ -1,18 +1,19 @@
 player_data = dict()
-score = list()
-player_data['name'] = str(input("Soccer player name: "))
+goals = list()
+player_data['name'] = str(input("Name: "))
 matches = int(input("How many matches does he played: "))
-for m in range(matches):
-    goal = int(input(f"How many goals did he score on {m} match: "))
-    score.append(goal)
-    player_data['goals'] = score[:]
-    player_data['total'] = sum(score)
-print("-=" * 30)
+for m in range(0, matches):
+    goals.append(int(input(f"how many goals on the {m} match? ")))
+player_data['goals'] = goals[:]
+player_data['total'] = sum(goals)
+del goals
+print("-="*30)
 print(player_data)
-print("-=" * 30)
+print("-="*30)
 for k, v in player_data.items():
     print(f"{k} has the value {v}")
-print("-=" * 30)
-print(f"the player {player_data['name']} has played {len(player_data['goals'])} matches")
-for e,g in enumerate(score):
-    print(f"      => On the {e} match he scored {g}")
+print("-="*30)
+print(f"The player {player_data['name']} played {len(player_data['goals'])} matches")
+for e,v in enumerate(player_data['goals']):
+    print(f"   => On match {e} he scored {v} goals")
+print(f"it was a total of {player_data['total']} goals")
