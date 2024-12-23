df5ac29aa620c4fd006a1dfd4786c1f2c0e09243
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-23 15:04:43 -0300

    Remaking challenge 95 about doing an upgrade of the challenge 93 but now with a menu that you can see specific data from each player

--- problems/class 19th/challenge95.py
+++ problems/class 19th/challenge95.py
@@ -1,36 +1,35 @@
-players = []
-goals = []
-data = {}
+player_data = dict()
+players = list()
+goals = list()
 while True:
-    data["name"] = str(input("Name: "))
-    data["matches"] = int(input("How many matches does he played? "))
-    sum_goals = 0
-    for m in range(0, data['matches']):
-        g = int(input(f"Match {m} goals: "))
-        sum_goals += g
-        goals.append(g)
-    data["sum_goal"] = sum_goals
-    data["goals"] = goals[:]
-    players.append(data.copy())
-    data.clear()
+    player_data.clear()
     goals.clear()
-    ask_continue = str(input("Continue? [y/n]: ")).strip()
+    player_data['name'] = str(input("Name: "))
+    matches = int(input("How many matches does he played: "))
+    for m in range(0, matches):
+        goals.append(int(input(f"how many goals on the {m} match? ")))
+    player_data['goals'] = goals[:]
+    player_data['total'] = sum(goals)
+    players.append(player_data.copy())
+    while True:
+        ask_continue = str(input("Continue? [y/n]: ")).strip()[0]
+        if ask_continue in "nNYy":
+            break
     if ask_continue in "nN":
         break
 print("-=" * 30)
 print(f"{'Code':<2} {'Name':<10} {'Goals':>3} {'Sum':>6}")
 print("-"*60)
 for e, p in enumerate(players):
-    print(f"{e:<2}  {p['name']:<10} {str(p['goals']):>3} {p['sum_goal']:>6}")
+    print(f"{e:<2}  {p['name']:<10} {str(p['goals']):>3} {p['total']:>6}")
 while True:
-    choise = int(input("Show data from which player? [999 to stop]: "))
-    for i, p in enumerate(players):
-        if choise == i:
-            print(f"---{p['name']}`s data: ")
-            for e, g in enumerate(players[i]['goals']):
-                print(f"  On match {e} he did {g} goals")
-    if choise > len(players):
-        print(f"ERROR! There's no player with the code {choise} place try again")
-    if choise == 999:
+    show_data = int(input("Show data from which player? (999 to stop): "))
+    for e, n in enumerate(players):
+        if show_data == e:
+            print(f"----{n['name']}'s data")
+            for e,v in enumerate(n['goals']):
+                print(f"on the {e} match he scored {v} goals")
+    if show_data > e and show_data != 999:
+        print(f"there is no player with the {show_data} code, pleace try again")
+    if show_data == 999:
         break
-          
