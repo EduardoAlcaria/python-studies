60289dc3e739e089ce5b283ce2f3c7b7c80c8998
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-23 10:54:55 -0300

    Remaking challenge 91 about doing a game that each player in a dictionary get a value,  and then show which one won based on a sorted dictionary

--- problems/class 19th/challenge91.py
+++ problems/class 19th/challenge91.py
@@ -1,10 +1,13 @@
 from random import randint
 from operator import itemgetter
-sPlayer = list()
-players = {'player1': randint(1, 6), 'player2': randint(1, 6), 'player3': randint(1, 6), 'player4': randint(1, 6)}
-for k,v in players.items():
-    print(f"{k} got {v}")
-sPlayer = sorted(players.items(), key=itemgetter(1), reverse=True)
-print("==== RANKING ====")
-for e, s in enumerate(sPlayer):
-    print(f"    => {e+1} place: {s[0]} with {s[1]}")
+players = {"Player1": randint(1, 6),
+           "Player2": randint(1, 6),
+           "Player3": randint(1, 6),
+           "player4": randint(1, 6)}
+for k, v in players.items():
+    print(f"the player {k} got {v}")
+print("======= RANKING =======")
+sorted_players = sorted(players.items(), key=itemgetter(1), reverse=True)
+for e, v in enumerate(sorted_players):
+    print(f"{e+1} Place: {v[0]}")
+
