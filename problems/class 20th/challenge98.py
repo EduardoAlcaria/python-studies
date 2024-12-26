1de5a04e553634595691a2d2d50b1b64a96f7200
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-26 08:05:14 -0300

    challenge 98 about doing a counter function that gets 3 values and do 3 counters, 1 from 10 to 0  jumping 2 numbers, from 0 to 10 jumping 1 number, and another custom counter

--- problems/class 20th/challenge98.py
+++ problems/class 20th/challenge98.py
@@ -1,19 +1,24 @@
 def count(b, e, s):
-    print("-="*30)
+    if s == 0:
+        s = 1
+    if s < 0:
+        s = abs(s)
+    print(f"{'-=' * 30}")
     print(f"count down from {b} to {e} on {abs(s)} on {abs(s)}")
-    for c in range(b, e+s, s):
-        print(f"{c}",end=" ")
-    print(f"\n{'-='*30}")
+    c = b
+    if b < e:
+        while c <= e:
+            print(f"{c}",end=" ")
+            c += s
+    if b > e:
+        while c >= e:
+            print(f"{c}",end=" ")
+            c -= s
+    print("END")
+    print(f"{'-='*30}")
 count(1, 10, 1)
 count(10, 0, -2)
 readB = int(input("Begin: "))
 readE = int(input("End: "))
 readS = int(input("Steps: "))
-if readS == 0:
-    readS = 1
-if readS < 0:
-     readS = abs(readS)
-if readB > readE:
-        count(readB, readE, -readS)
-else:
-     count(readB, readE, readS)
+count(readB, readE, readS)
