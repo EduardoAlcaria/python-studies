b0f4e15aa6377cda1e61cfc477c4cea23993b4e7
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-26 08:55:22 -0300

    challenge 100 about doing 2 functions, one to generate 5 values, and another to adding them

--- problems/class 20th/challenge100.py
+++ problems/class 20th/challenge100.py
@@ -1,19 +1,19 @@
 from random import randint
 num = list()
 def rannum():
-    print(f"Generating 5 numbers: ",end=" ")
+    print(f"Generating 5 numbers:",end=" ")
     for c in range(5):
-        r = randint(0, 10)
+        r = randint(1, 10)
         print(f"{r}", end=" ")
         num.append(r)
-    print("READY")
+    print("DONE")
 def sEven(n):
+    rannum()
     s = 0
-    print(f"adding {num} we have ",end="")
+    print(f"adding only the even numbers in this sequence {num} we have ",end="")
     for c in n:
         if c % 2 == 0:
             s += c
     print(f"{s}",end=" ")
     print()
-rannum()
 sEven(num)
