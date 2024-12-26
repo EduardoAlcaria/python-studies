4254ee0fb2fb3fe9598f78f7fad3cc852364837f
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-26 08:37:43 -0300

    challenge 99 about passing some values to a function to do an analysis about which number is the highest one and how many elements the user gave to it

--- problems/class 20th/challenge99.py
+++ problems/class 20th/challenge99.py
@@ -1,10 +1,16 @@
 def major(* num):
+    h = 0
+    for e, c in enumerate(num):
+        if e == 0:
+            h = c
+        else:
+            if c > h:
+                h = c
     print("-=" * 30)
     for v in num:
         print(f"{v}",end=" ")
     print(f"you have typed {len(num)} values")
-    print(f"the higher value was {max(num)}")
-    print("-=" * 30)
+    print(f"the higher value was {h}")
 major(2, 9, 4, 5, 7, 1)
 major(4, 7, 0)
 major(0)
