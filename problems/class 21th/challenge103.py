91f02c9507a978a93607fe9b1b5cdd5e141fac73
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-27 16:43:15 -0300

    challenge 103 about doing a function that get a 2 parameters and print a formated version of it, but if the user dont pass the parameters, the user will get the answer the same way, but with a place holder values

--- problems/class 21th/challenge103.py
+++ problems/class 21th/challenge103.py
@@ -1,7 +1,9 @@
 def player(name="", goals=0 ):
     if name == "":
         name = "<unknown>"
-    if goals == "":
+    if goals.isnumeric():
+        goals = int(goals)
+    else:
         goals = 0
     print(f"the player {name} did {goals} goal(s) at the tournament")
 player_read = str(input("Player's name: "))
