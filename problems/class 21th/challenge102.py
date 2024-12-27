f2f4136f5206937f1c9f00b16178ef783b58f224
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-27 16:32:30 -0300

    challenge 102 about doing a factorial calculator with can show the calculation processes if you pass the show parameter show=True, else it will just return the result

--- problems/class 21th/challenge102.py
+++ problems/class 21th/challenge102.py
@@ -4,11 +4,9 @@ def fact(n, show=False):
         f *= c
     if show == True:
         for c in range(n, 0, -1):
-            print(f"{c}", end="")
-            if c>1:
-                print(" x ",end="")
-            else:
-                print(" = ",end="")
+            print(f"{c} x ", end="")
+            if c == 1:
+                print("= ",end="")
         return f
     else:
         return f
