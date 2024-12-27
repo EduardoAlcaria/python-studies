f5f952a628984b41e9822e0c88ef338f0604820b
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-27 16:22:29 -0300

    challenge 101 is about doing a function that check if you need top vote, it returns 3 strings ("DENIED, "OBLIGATORY" and "OPTIONAL") if you attent the age requirements

--- problems/class 21th/challenge101.py
+++ problems/class 21th/challenge101.py
@@ -1,13 +1,14 @@
-from datetime import datetime
+age = 0
 def needtovote(y):
-    global age
-    if age < 18:
-        return "DENIED"
+    from datetime import datetime
+    global age 
+    age = datetime.today().year - y
+    if age >= 16 and age < 18 or age > 65:
+        return "OPTIONAL"
     if age >= 18 and age < 65:
         return "OBLIGATORY"
     else:
-        return "OPTIONAL"
+        return "DENNIED"
 yearB = int(input("Year that you were born: "))
-age = datetime.today().year - yearB
 aVote = needtovote(yearB)
-print(f"with {age} the vote right is {aVote}")
+print(f"with {age} years old the vote right is {aVote}")
