7f867bfdee003cfa8c0f0e19c74eeaace01bf0fe
EduardoAlcariaclear <eduardoalcarialopes1@outlook.com>
2024-12-27 17:14:16 -0300

    challenge 105 about doing a function that analizes grades and put them into a dictionary

--- problems/class 21th/challenge105.py
+++ problems/class 21th/challenge105.py
@@ -1,32 +1,16 @@
 def grades(* data, sit=False):
     g = dict()
     g['total'] = len(data)
-    major = 0
-    minor = 0
-    for e, m in enumerate(data):
-        if e == 0:
-            minor = m
-            major = m
-        else:
-            if m > major:
-                major = m
-            if m < minor:
-                minor = m
-    g['major'] = major
-    g['minor'] = minor
-    av = 0
-    for a in data:
-        av += a
-    g['avarege'] = av/len(data)
-    if sit == False:
-        return g
-    else:
+    g['major'] = max(data)
+    g['minor'] = min(data)
+    g['avarege'] = sum(data)/g['total']
+    if sit:
         if g['avarege'] >= 7:
-            g['sitation'] = "GOOD"
-        if g['avarege'] >= 5 and g['avarege'] < 7:
-            g['sitation'] = "REASONABLE"
-        if g['avarege'] >= 0 and g['avarege'] < 5:
-            g['sitation'] = "BAD"
-        return g
-r = grades(5.5, 9.5, 10, 6.5, sit=True)
+            g['situation'] = "GOOD"
+        elif g['avarege'] >= 5:
+            g['situation'] = "REASONABLE"
+        else:
+            g['situation'] = "BAD"
+    return g
+r = grades(5.5, 2.5, 8.5, sit=True)
 print(r)
