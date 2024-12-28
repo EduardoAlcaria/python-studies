e07fcfe79c4ad1407cd9db2d9f2badb896a6ab09
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-12-28 15:13:22 -0300

    reuploading files

---
+++ problems/class 22th/challenge108.py
@@ -0,0 +1,6 @@
+from challenge108m import coin
+num = float(input("Type the price: "))
+print(f"the half part of {num} is {coin.coin()}")
+print(f"the double part of {num} is {coin.double(num)}")
+print(f"increasing 10%, we have {coin.increase(num, 10):.2f}")
+print(f"decresing 13%, we have {coin.decrease(num, 13)}")
---
+++ problems/class 22th/challenge108m/coin.py
@@ -0,0 +1,8 @@
+def double(c):
+    return c * 2
+def half(c):
+    return c / 2
+def increase(c, p):
+    return ((p/100)*c) + c
+def decrease(c, p):
+    return c - ((p/100)*c)
